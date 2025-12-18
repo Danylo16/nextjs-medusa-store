83616f0912c1d7a61f04cb00fe4b0b7b625f147d
Danylo Zubrii <zdanilevich@gmail.com>
2025-12-18 20:41:17 +0200

    fix: update image remote patterns and improve CORS configuration

--- backend-storefront/next.config.js
+++ backend-storefront/next.config.js
@@ -26,6 +26,10 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "<backend>.up.railway.app",
+      },
+      {
+        protocol: "https",
         hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
       },
       {
